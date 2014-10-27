1ac01eed01aea3c21d85d4093caf0d67de155bea
Alec Fekete <fekete2@illinois.edu>
2014-10-26 22:20:05 -0500

    added RESTful API thing and mongodb connection

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var xml2js = require('xml2js');
-var async = require('async');
-var h5bp = require('h5bp');
-var fs = require('fs');
-var util = require('util');
-var parse = require('./js/svn_parser.js');
+var express = require('express'),
+    mongoskin = require('mongoskin'),
+    bodyParser = require('body-parser'),
+    xml2js = require('xml2js'),
+    async = require('async'),
+    h5bp = require('h5bp'),
+    fs = require('fs'),
+    util = require('util'),
+    parse = require('./js/svn_parser.js');
+
+var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true});
 
 //create express server
 var app = express();
+app.use(bodyParser());
 
 // Create xml2js parser objects. One object per file to prevent overwriting errors
 var list_parser = new xml2js.Parser();
@@ -51,13 +56,51 @@ app.use(h5bp({ root: __dirname + '/views' }));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
+app.param('collectionName', function(req, res, next, collectionName) {
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+app.get('/collections/:collectionName', function(req, res, next) {
+    req.collection.find({}, {limit:10, sort:[['_id', -1]]}).toArray(function(e, results) {
+        if (e) return next(e);
+        res.send(results)
+    })
+});
+app.post('/collections/:collectionName', function(req, res, next) {
+    req.collection.insert(req.body, {}, function(e, results) {
+        if (e) return next(e);
+        res.send(results)
+    })
+});
+app.get('/collections/:collectionName/:id', function(req, res, next) {
+    req.collection.findById(req.params.id, function(e, result) {
+        if (e) return next(e);
+        res.send(result)
+    })
+});
+app.put('/collections/:collectionName/:id', function(req, res, next) {
+    req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false},
+    function(e, result) {
+        if (e) return next(e);
+        res.send((result===1)?{msg:'success'}:{msg:'error'})
+    })
+});
+app.delete('/collections/:collectionName/:id', function(req, res, next) {
+    req.collection.removeById(req.params.id, function(e, result) {
+        if (e) return next(e);
+        res.send((result===1)?{msg:'success'}:{msg:'error'})
+    })
+});
+
+
 // Set what to render when a get request is made at /
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
 	res.render('index', { title: 'CS242 Portfolio', project_list: project_list})
 });
 
-// Set up the server to listen at the correct port (provided by heroku or 4567 on local host)
-var server = app.listen(process.env.PORT || 4567, function () {
+// Set up the server to listen at the correct port (provided by heroku or 3000 on local host)
+var server = app.listen(process.env.PORT || 3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
